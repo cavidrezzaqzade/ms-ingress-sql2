ea5aae9919d5fb390c85ff2e0cf9f1de6f210118
cavidrezzaqzade <cavidrazzagzade@gmail.com>
2023-05-30 17:41:35 +0400

    Initial commit

---
+++ src/main/java/az/ingress/sql/MsIngressSql2Application.java
@@ -0,0 +1,13 @@
+package az.ingress.sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsIngressSql2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsIngressSql2Application.class, args);
+    }
+
+}
---
+++ src/test/java/az/ingress/sql/MsIngressSql2ApplicationTests.java
@@ -0,0 +1,13 @@
+package az.ingress.sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsIngressSql2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
